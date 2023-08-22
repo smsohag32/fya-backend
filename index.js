6ab16ee2e20fb8fec061bca9c35a66d56a1f448b
SM Sohag <sohagsheik32@gmail.com>
2023-08-22 17:38:10 +0600

    update

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 const app = express();
-const { connectDB } = require('./config/dbConfig');
+const { connectDB } = require("./config/dbConfig");
 const port = process.env.PORT || 3000;
-const workshopRoutes = require('./routes/workshopRoute.js')
+const workshopRoutes = require("./routes/workshopRoute.js");
 const morgan = require("morgan");
 
 // setting middlewares
-app.use(cors());
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(morgan("dev"));
 
-app.get('/', (req, res) => {
-    res.send('server is running')
+app.get("/", (req, res) => {
+  res.send("server is running");
 });
 
-
-app.use('/api/v1/auth', workshopRoutes);
-
-
+app.use("/api/v1/auth", workshopRoutes);
 
 app.listen(port, async () => {
-   console.log('server is running');
-    await connectDB();
+  console.log("server is running");
+  await connectDB();
 });
-
-
