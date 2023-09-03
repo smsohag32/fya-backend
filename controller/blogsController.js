cbd139d7b09dab67ac50cd54e5efd8f553c7a95a
smsohag32 <sohagsheik32@gmail.com>
2023-09-03 17:32:33 +0600

    update

--- controller/blogsController.js
+++ controller/blogsController.js
@@ -36,5 +36,22 @@ const likeCount = async (req, res) => {
 }
 
 
+const addComment = async (req, res) => {
+     try {
+       const id = req.params.id;
+       const newComment = req.body;
+       const update =  { $push: { comments: newComment } }
+       const options = { new: true, upsert: true };
+       const updatedBlog = await blogsInfo.findByIdAndUpdate(id, update, options);
+        if (!updatedBlog) {
+            return res.status(404).send('Blog not found');
+        }
+        res.send(updatedBlog);
+         } catch (error) {
+        res.status(500).send(error.message);
+        }
+}
 
-module.exports = { getBlogs, getBlog, likeCount };
+
+
+module.exports = { getBlogs, getBlog, addComment ,likeCount};
